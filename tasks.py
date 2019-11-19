0d62d51757ac0d5160d35c43d06f77c75503b413
Likbjorn <dbegal@mail.ru>
2019-11-19 21:19:41 +0300

    Add task send_message_yt

--- tasks.py
+++ tasks.py
@@ -66,3 +66,36 @@ def send_message_yt(
         youtube_url=youtube_url,
         desired_resolution=desired_resolution,
     ))
+
+
+@app.task(name='celery_tasks.send_message_yt')
+def send_message_yt(
+        channel_id: int,
+        author_id: int,
+        youtube_url: str,
+        desired_resolution: int = 1080
+):
+    async def _wait_for_yt(
+            channel_id: int,
+            author_id: int,
+            youtube_url: str,
+            desired_resolution: int = 1080
+    ):
+        """Delay message with YT url until video gets processed to desired resolution."""
+        message = youtube_url
+        while True:
+            streams = YouTube(youtube_url).streams.all()
+            for stream in streams:
+                if stream.resolution >= desired_resolution:
+                    target_dt = dt.datetime.utcnow()
+                    await send_message(channel_id, author_id, message, target_dt)
+                    return
+            await asyncio.sleep(config.SCHEDULER_SLEEP_TIME)
+            # TODO: add timeout for videos which never reach target quality
+
+    asyncio.run(_wait_for_yt(
+        channel_id=channel_id,
+        author_id=author_id,
+        youtube_url=youtube_url,
+        desired_resolution=desired_resolution,
+    ))
