549b420eb35f4ebb8be85e023a8c1fcf51947653
Likbjorn <dbegal@mail.ru>
2019-11-21 22:50:12 +0300

    Save scheduled messages to db Mess up messed up send_yt_message

--- tasks.py
+++ tasks.py
@@ -6,6 +6,8 @@ from celery import Celery
 from pytube import YouTube
 
 import config
+import models
+from db import session
 from utils import get_discord_client
 
 app = Celery(main='messages', broker=config.CELERY_BROKER_URL, backend=config.CELERY_RESULT_BACKEND)
@@ -16,14 +18,20 @@ def send_message(
         channel_id: int,
         author_id: int,
         message: str,
+        target_dt: dt.datetime,
 ):
     async def _send_message(
             channel_id: int,
             author_id: int,
             message: str,
+            target_dt: dt.datetime,
     ):
         async with get_discord_client() as client:
             client.get_channel(channel_id)
+            db_instance = models.ScheduledMessage(message=message, send_ts=target_dt)
+            session.add(db_instance)
+            session.commit()
+            session.close()
             author = client.get_user(author_id)
             msg = f'**{author.mention} said:** {message}'
             await client.get_channel(channel_id).send(msg)
@@ -32,6 +40,7 @@ def send_message(
         channel_id=channel_id,
         author_id=author_id,
         message=message,
+        target_dt=target_dt,
     ))
 
 
@@ -42,60 +51,12 @@ def send_message_yt(
         youtube_url: str,
         desired_resolution: int = 1080
 ):
-    async def _wait_for_yt(
-            channel_id: int,
-            author_id: int,
-            youtube_url: str,
-            desired_resolution: int = 1080
-    ):
-        """Delay message with YT url until video gets processed to desired resolution."""
-        message = youtube_url
-        while True:
-            streams = YouTube(youtube_url).streams.all()
-            for stream in streams:
-                if stream.resolution >= desired_resolution:
-                    target_dt = dt.datetime.utcnow()
-                    await send_message(channel_id, author_id, message, target_dt)
-                    return
-            await asyncio.sleep(config.SCHEDULER_SLEEP_TIME)
-            # TODO: add timeout for videos which never reach target quality
-
-    asyncio.run(_wait_for_yt(
-        channel_id=channel_id,
-        author_id=author_id,
-        youtube_url=youtube_url,
-        desired_resolution=desired_resolution,
-    ))
-
-
-@app.task(name='celery_tasks.send_message_yt')
-def send_message_yt(
-        channel_id: int,
-        author_id: int,
-        youtube_url: str,
-        desired_resolution: int = 1080
-):
-    async def _wait_for_yt(
-            channel_id: int,
-            author_id: int,
-            youtube_url: str,
-            desired_resolution: int = 1080
-    ):
-        """Delay message with YT url until video gets processed to desired resolution."""
-        message = youtube_url
-        while True:
-            streams = YouTube(youtube_url).streams.all()
-            for stream in streams:
-                if stream.resolution >= desired_resolution:
-                    target_dt = dt.datetime.utcnow()
-                    await send_message(channel_id, author_id, message, target_dt)
-                    return
-            await asyncio.sleep(config.SCHEDULER_SLEEP_TIME)
-            # TODO: add timeout for videos which never reach target quality
-
-    asyncio.run(_wait_for_yt(
-        channel_id=channel_id,
-        author_id=author_id,
-        youtube_url=youtube_url,
-        desired_resolution=desired_resolution,
-    ))
+    """Send message with YouTube url if it is of target quality or higher."""
+    message = youtube_url
+    streams = YouTube(youtube_url).streams.all()
+    for stream in streams:
+        if stream.resolution >= desired_resolution:
+            target_dt = dt.datetime.utcnow()
+            send_message.apply_async(
+                args=(channel_id, author_id, message, target_dt),
+            )
