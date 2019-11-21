489355362bfc32346b92d5ca39d90dab601e491f
Likbjorn <dbegal@mail.ru>
2019-11-21 22:57:30 +0300

    Save delayed message to db before initiating sending task

--- tasks.py
+++ tasks.py
@@ -4,8 +4,6 @@ import datetime as dt
 from celery import Celery
 
 import config
-import models
-from db import session
 from utils import get_discord_client
 
 
@@ -17,20 +15,14 @@ def send_message(
         channel_id: int,
         author_id: int,
         message: str,
-        target_dt: dt.datetime,
 ):
     async def _send_message(
             channel_id: int,
             author_id: int,
             message: str,
-            target_dt: dt.datetime,
     ):
         async with get_discord_client() as client:
             client.get_channel(channel_id)
-            db_instance = models.ScheduledMessage(message=message, send_ts=target_dt)
-            session.add(db_instance)
-            session.commit()
-            session.close()
             author = client.get_user(author_id)
             msg = f'**{author.mention} said:** {message}'
             await client.get_channel(channel_id).send(msg)
@@ -39,5 +31,4 @@ def send_message(
         channel_id=channel_id,
         author_id=author_id,
         message=message,
-        target_dt=target_dt,
     ))
